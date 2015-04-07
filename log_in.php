fa941668cc122062d8175d8f0b8bc275375b5896
Nippeli87 <tuukka.nisso@metropolia.fi>
2015-04-07 20:22:53 +0300

    Update log_in.php

--- log_in.php
+++ log_in.php
@@ -1,13 +1,38 @@
-<?php 
+
+<?php
 session_start();
 require_once('yhteiset/dbYhteys.php');
 require_once('yhteiset/funktiot.php');
 require_once('yhteiset/dbFunctions.php');
-//SSLon();
+
+SSLon();
+?>
+<?php
 //Login -> $_SESSION['kirjautunut']='loggedIn'	
-if(!empty($_POST['user'])&&!empty($_POST['pwd'])){
-	if(check_user($_POST['user'], $_POST['pwd'], $DBH)){
-		 $_SESSION['kirjautunut'] = 'loggedIn';
+if(!empty($_POST['username'])&&!empty($_POST['pwd'])){
+	if(check_user($_POST['username'], $_POST['pwd'], $DBH)){
+		$_SESSION['kirjautunut'] = 'loggedIn';
+		$userInfoSql=("
+			SELECT
+				u.userID
+			FROM
+				a_kayttaja u
+			WHERE
+				u.username='".$_POST['username']."'"
+			);
+	//echo("UINFO SQUEEL".$userInfoSql);
+	
+		$STH=$DBH->query($userInfoSql);
+	$STH->setFetchMode(PDO::FETCH_ASSOC);
+try{
+	while($row=$STH->fetch()){
+		$_SESSION['user']=$row['userID'];   //KÄYTTÄJÄN ID SESSIOTA VARTEN MUUTTUJASSA $_SESSION['user']
+		//echo($_SESSION['user']);			// JOKAISELLE SIVULLE LAITETTAVA session_start(); ALKUUN ETTÄ TOIMII^^
+	}
+}catch(PDOException $e){
+	echo "Jotain meni pieleen :(";
+	file_put_contents('../../loki/PDOErrors.txt', $e->getMessage()."\n",FILE_APPEND);
+}
 	} else {
 		echo '<script>alert("Login Failure");</script>';
 	}
@@ -18,35 +43,182 @@ if($_GET['action'] == 'logout'){
 	session_destroy();
 }
 //Tarkastetaan sisäänkirjatuminen, jos inessä näytetään "Logout"
-if ($_SESSION['kirjautunut'] == 'loggedIn'):
-?>
-<a href="<?php echo $_SERVER['PHP_SELF']; ?>?action=logout">Logout</a>
+		  ?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="./css.css"/>
+		<!--<script src="jquery-2.1.1.min.js"></script>
+		<script src="jqueryscripti.js"></script>-->
+		
+		<link href="http://cdnjs.cloudflare.com/ajax/libs/video.js/4.12.3/video-js.css" rel="stylesheet"> 
+		
+		<script src="js/video.js"></script>
+		<script src="js/wavesurfer.min.js"></script>
+		<script src="js/videojs.wavesurfer.js"></script>
+		<script src="js/funktioita.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>    </head>
+<body>
+
+<div class="top">
+    <h3>
+    Aaniblogi testitesti winscptest
+        </h3>
+</div>
+
+<div class="main">
+	<div class="logtitle">
+		<div class="titlediv">
+			<h1>
+			Aaniblogi
+				</h1>
+			</div>
+		<div class="login" id="login">
+				<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    			<input type="text" name="username" placeholder="username" id="user"/>
+				<input type="password" name="pwd" placeholder="password" id="pwd"/>
+				<input type="submit" name="kirjaudu" value="Login" class="button" id="loggaus" />
+			</div>
+<?php if ($_SESSION['kirjautunut'] == 'loggedIn'):
+	?>
+	<script>
+	$("#user").hide();
+	$("#pwd").hide();
+	$("#loggaus").replaceWith('<a href="<?php echo $_SERVER[´PHP_SELF´]; ?>?action=logout">Logout</a>'); 
+    </script>
+
 <?php
 //Else Login-palikka
-else: 
+//else: 
 ?>
-<a href="#" data-reveal-id="login">Login</a>
+<!--<a href="#" data-reveal-id="login">Login</a> -->
 
 <?php
 // lopeta if
 endif;
-		  ?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Ownage</title>
-	<link href="base.css" rel="stylesheet" type="text/css">
-	<link href='https://fonts.googleapis.com/css?family=Oswald:700&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
-	<link href='https://fonts.googleapis.com/css?family=Abel' rel='stylesheet' type='text/css'>
-	<meta charset="utf-8">
-</head>
-<body>
-<div id="login" class="reveal-modal">
-  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-    <input type="text" name="user" placeholder="insert username" />
-    <input type="password" name="pwd" placeholder="insert password" />
-    <input type="submit" name="login" value="Login" class="button" />
-  </form>
-  <a class="close-reveal-modal"></a></div>
-</body>
-</html>
+?>
+	<div class="aiheet_nav">
+		<div class="aihe_1">
+			aihe1
+			</div>
+		<div class="aihe_2">
+			aihe2
+			</div>
+		<div class="aihe_3">
+			aihe3
+			</div>
+		<div class="aihe_4">
+			aihe4
+			</div>
+		</div>
+	<div class="searchbar">
+		<input type="text" name="search" placeholder="Search... " />
+     	<input type="submit" name="go" value="Go" class="button" />
+	 </div>
+	 
+	<div class="keskustelut">
+		<div class="kesk_1_1">
+			<audio id="myAudio" class="video-js vjs-default-skin"></audio>
+
+		<script>
+		var player = videojs("myAudio",
+		{
+			controls: true,
+			autoplay: false,
+			loop: false,
+			width: 360,
+			height: 110,
+			plugins: {
+				wavesurfer: {
+					src: "audio/test.mp3",
+					msDisplayMax: 10,
+					waveColor: "grey",
+					progressColor: "black",
+					cursorColor: "black",
+					hideScrollbar: true
+				}
+			}
+		});
+		// change player background color
+		player.el().style.backgroundColor = "#FFEEFF";
+		</script>
+			</div>
+		<div class="kesk_1_2">
+		Additionaly it provides some tools useful in real-life cases. Such as the ability for the user to mute the sound. Its is useful when the user is at the office or any place where it isn't polite to have a loud computer :) 
+
+		<audio id="myAudio2" class="video-js vjs-default-skin"></audio>
+
+		<script>
+		var player = videojs("myAudio2",
+		{
+			controls: true,
+			autoplay: false,
+			loop: false,
+			width: 360,
+			height: 110,
+			plugins: {
+				wavesurfer: {
+					src: "audio/test.mp3",
+					msDisplayMax: 10,
+					waveColor: "grey",
+					progressColor: "black",
+					cursorColor: "black",
+					hideScrollbar: true
+				}
+			}
+		});
+		// change player background color
+		player.el().style.backgroundColor = "#FFEEFF";
+		</script>
+			</div>
+		<div class="kesk_1_3">
+		<audio id="myAudio3" class="video-js vjs-default-skin"></audio>
+
+		<script>
+		var player = videojs("myAudio3",
+		{
+			controls: true,
+			autoplay: false,
+			loop: false,
+			width: 360,
+			height: 110,
+			plugins: {
+				wavesurfer: {
+					src: "audio/test.mp3",
+					msDisplayMax: 10,
+					waveColor: "grey",
+					progressColor: "black",
+					cursorColor: "black",
+					hideScrollbar: true
+				}
+			}
+		});
+		// change player background color
+		player.el().style.backgroundColor = "#FFEEFF";
+		</script>
+		This helper provides a main line out with the good practices from "Developing Game Audio with the Web Audio API" on html5rocks. So it provides a clipping detection and a dynamic compressor to reduce clipping to improve sound quality.
+
+Additionaly it provides some tools useful in real-life cases. Such as the ability for the user to mute the sound. Its is useful when the user is at the office or any place where it isn't polite to have a loud computer :) 
+			</div>
+		</div>
+		<div class="kesk_2_1">
+			<iframe width="380" height="214" src="https://www.youtube.com/embed/RVB7uSPr4Ns" frameborder="0" allowfullscreen></iframe>
+			</div>
+		<div class="kesk_2_2">
+			<iframe width="380" height="214" src="https://www.youtube.com/embed/RVB7uSPr4Ns" frameborder="0" allowfullscreen></iframe>
+			</div>
+		<div class="kesk_2_3">
+		This helper provides a main line out with the good practices from "Developing Game Audio with the Web Audio API" on html5rocks. So it provides a clipping detection and a dynamic compressor to reduce clipping to improve sound quality.
+			<iframe width="380" height="214" src="https://www.youtube.com/embed/RVB7uSPr4Ns" frameborder="0" allowfullscreen></iframe>
+			</div>
+	<div class="bottom">
+		tamasivu on luotu huonosti http//:::ok.web.ko
+		</div>
+</div>
+</body></html>
+
+
+
+
