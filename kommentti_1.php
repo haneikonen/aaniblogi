eff93594df5447ee0c63045d6f42219db3948a2e
Nippeli87 <tuukka.nisso@metropolia.fi>
2015-04-15 09:45:15 +0300

    Create kommentti_1.php

---
+++ kommentti_1.php
@@ -0,0 +1,17 @@
+<?php 
+$count = $DBH->prepare("SELECT COUNT(julkaisu) FROM a_kommentti WHERE julkaisu = ".$mostPopularID[0].";");
+$count->execute();
+$count_data = $count->fetch();
+$cnt_rows = $count_data['COUNT(julkaisu)'];
+
+$sql = "SELECT user_ID, kommentti, audio FROM a_kommentti WHERE julkaisu = ".$mostPopularID[0].";";
+$result = $DBH->query($sql);				
+if($cnt_rows > 0){
+	for($i = 1; $i < $cnt_rows; $i++){
+		$row = $result-> fetch();
+		echo('<br><td>'.$row['kommentti'].'</td></br>');
+		}
+	} else{
+		echo('no comments');
+	}
+					?>
