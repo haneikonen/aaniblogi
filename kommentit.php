e1125696e32d47edaa7a60196570bf2e7f6f8fde
haneikonen <haneikonen@gmail.com>
2015-05-11 19:40:04 +0300

    Update kommentit.php

--- kommentit.php
+++ kommentit.php
@@ -1,34 +1,31 @@
+
 <?php
 //JSON_ENCODE -> voidaan erotella käyttäjä, kommentti ja audio-urli
 //javascriptin puolella erikseen
-
 require_once('../yhteiset/dbYhteys.php');
 $k=$_POST['q'];
 //$k=1;
 $kommenttilista=
 "SELECT
- a.kommentti,
- a.audio,
- k.username,
- a.posted
+a.kommentti,
+a.audio,
+k.username,
+a.posted
 FROM
- a_kommentti a,
- a_kayttaja k
+a_kommentti a,
+a_kayttaja k
 WHERE
- a.julkaisu=".$k." AND
- a.user_ID=k.userID;";
-
+a.julkaisu=".$k." AND
+a.user_ID=k.userID;";
 $STH=$DBH->query($kommenttilista);
 $STH->setFetchMode(PDO::FETCH_ASSOC);
-
 $kommentti=[];
 $kommentit=[];
 $kommentit["comments"]=[];
-
 try{
 	while($row=$STH->fetch()){
-		$kommentti=array('username'=>$row['username'],'audio'=>$row['audio'],'url'=>$row['url'],'kommentti'=>$row['kommentti'],'time'=>date('G:i j.n.Y',strtotime($row['posted'])));
-		array_push($kommentit["comments"],$kommentti);
+	$kommentti=array('username'=>$row['username'],'audio'=>$row['audio'],'kommentti'=>$row['kommentti'],'time'=>date('G:i j.n.Y',strtotime($row['posted'])));
+	array_push($kommentit["comments"],$kommentti);
 	}
 	echo(json_encode($kommentit));
 }catch(PDOException $e){
@@ -36,11 +33,11 @@ try{
 	file_put_contents('../../loki/PDOErrors.txt', $e->getMessage()."\n",FILE_APPEND);
 }
 /*try{
-	while($row=$STH->fetch()){
-		echo('<p>'.$row['kommentti'].'</p><p>'.$row['username'].'</p>');
-	}
+while($row=$STH->fetch()){
+echo('<p>'.$row['kommentti'].'</p><p>'.$row['username'].'</p>');
+}
 }catch(PDOException $e){
-		echo "Jotain meni pieleen :(";
-		file_put_contents('../../loki/PDOErrors.txt', $e->getMessage()."\n",FILE_APPEND);
+echo "Jotain meni pieleen :(";
+file_put_contents('../../loki/PDOErrors.txt', $e->getMessage()."\n",FILE_APPEND);
 }*/
 ?>
