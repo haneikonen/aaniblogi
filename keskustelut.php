2f379efdf813e8209d315d2b3b8161e3c0b84514
haneikonen <haneikonen@gmail.com>
2015-05-11 19:40:46 +0300

    Update keskustelut.php

--- keskustelut.php
+++ keskustelut.php
@@ -1,43 +1,40 @@
 <?php
 require_once('../yhteiset/dbYhteys.php');
 $z=$_POST['q'];
-
 //$z=1;
-
 $keskustelu=[];
 $keskustelut=[];
 $keskustelulista=
-"SELECT 
-	j.title,
-	j.ID,
-	j.url
-FROM 
-	a_julkaisu j
-WHERE 
-	j.aihealue=".$z."
-	;";
+"SELECT
+j.title,
+j.ID,
+j.url
+FROM
+a_julkaisu j
+WHERE
+j.aihealue=".$z."
+;";
 $STH=$DBH->query($keskustelulista);
 $STH->setFetchMode(PDO::FETCH_ASSOC);
 /*try{
-	while($row=$STH->fetch()){
-		echo('<a href="#" id='.$row['ID'].' class="comment">'.$row['title'].'</a><br \>');
-	}
+while($row=$STH->fetch()){
+echo('<a href="#" id='.$row['ID'].' class="comment">'.$row['title'].'</a><br \>');
+}
 }
 */
 $keskustelut["om"]=[];
-	try{
-		while($row=$STH->fetch()){
-			$keskustelu=array('title' => $row['title'], 'id' => $row['ID']);
-			//$keskustelut["om"]=$keskustelu;
-			array_push($keskustelut["om"],$keskustelu);
-		}
-		//echo("ECHO ".json_encode($keskustelu));
-		//print_r($keskustelu);
-		echo(json_encode($keskustelut));
-		
-	}
+try{
+while($row=$STH->fetch()){
+$keskustelu=array('title' => $row['title'], 'id' => $row['ID'], 'url'=>$row['url']);
+//$keskustelut["om"]=$keskustelu;
+array_push($keskustelut["om"],$keskustelu);
+}
+//echo("ECHO ".json_encode($keskustelu));
+//print_r($keskustelu);
+echo(json_encode($keskustelut));
+}
 catch(PDOException $e){
-	echo "Jotain meni pieleen :(";
-	file_put_contents('../../loki/PDOErrors.txt', $e->getMessage()."\n",FILE_APPEND);
+echo "Jotain meni pieleen :(";
+file_put_contents('../../loki/PDOErrors.txt', $e->getMessage()."\n",FILE_APPEND);
 }
 ?>
